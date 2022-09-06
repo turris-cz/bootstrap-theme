turris-cz/bootstrap-theme

commit b304f3ee08eef4477700585ba6960d1da4da5cb4

Integrate fonts into Makefile

Makefile
@@ -23,6 +23,7 @@ prepare:
 .PHONY: install
 install:
 	install -d '$(INSTALL_DIR)'
+	install -m 644 fonts/* '$(INSTALL_DIR)'
 	install -m 644 darkmode_head.js darkmode_body.js '$(INSTALL_DIR)'
 	install -m 644 favicon-black.png favicon-white.png '$(INSTALL_DIR)'
 	install -m 644 logo-black.svg logo-white.svg '$(INSTALL_DIR)'

commit cb4db1f9a7b37f6cbbcc0bcdf5137a0f0331dddb

Makefile: move fonts to its subfolder
This moves folder fonts inside root directory to the subfolder fonts in
installation dir, because .css files expects it there
(https://gitlab.nic.cz/turris/bootstrap-theme/-/commit/5690de55a906d01df7602c45df3a7e027c2d894b)

Fixes: b304f3ee08eef4477700585ba6960d1da4da5cb4 ("Integrate fonts into
Makefile")

Makefile
@@ -23,12 +23,13 @@ prepare:
 .PHONY: install
 install:
 	install -d '$(INSTALL_DIR)'
-	install -m 644 fonts/* '$(INSTALL_DIR)'
 	install -m 644 darkmode_head.js darkmode_body.js '$(INSTALL_DIR)'
 	install -m 644 favicon-black.png favicon-white.png '$(INSTALL_DIR)'
 	install -m 644 logo-black.svg logo-white.svg '$(INSTALL_DIR)'
 	install -m 644 bootstrap.bundle.js bootstrap.bundle.js.map '$(INSTALL_DIR)'
 	install -m 644 bootstrap.bundle.min.js bootstrap.bundle.min.js.map '$(INSTALL_DIR)'
+	install -d '$(INSTALL_DIR)/fonts'
+	install -m 644 fonts/* '$(INSTALL_DIR)/fonts'
 
 .PHONY: clean
 clean::